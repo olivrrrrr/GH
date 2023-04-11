85c77847047e011d87e9a1ebf28df9c32d7d2fe9
olivrrrrr <oliverekwalla@gmail.com>
2023-04-11 21:42:20 +0100

    set-up UI tests

--- GHUITests/GHUITests.swift
+++ GHUITests/GHUITests.swift
@@ -10,24 +10,50 @@ import XCTest
 final class GHUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    private var searchFollowersButton: XCUIElement {
+        return XCUIApplication().buttons["Search followers"]
+    }
+    
+    private var usernameTextField: XCUIElement {
+        return XCUIApplication().textFields["Enter a username"]
+    }
+    
+    func whenISelectSearchFollowersButton() {
+        XCTContext.runActivity(named: #function) { _ in
+            searchFollowersButton.tap()
+        }
+    }
+    
+    func whenITapUsernameFieldAndWriteDetails() {
+        XCTContext.runActivity(named: #function) {_ in
+            usernameTextField.tap()
+            usernameTextField.typeText("olivrrrrr\n")
+        }
+    }
+    
+    func thenIShouldSeeSearchFollowersButton() {
+        XCTAssertTrue(searchFollowersButton.exists)
+    }
+    
+    func thenIShouldSeeUsernameTextField() {
+        XCTAssertTrue(usernameTextField.exists)
     }
 
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        thenIShouldSeeUsernameTextField()
+        whenITapUsernameFieldAndWriteDetails()
+    
+        XCUIApplication().collectionViews.firstMatch
     }
 
     func testLaunchPerformance() throws {
